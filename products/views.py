2e360e0d27ff486c37a17cac810f2b53259ec245
stephenbeese <stephenbeese1@live.co.uk>
2023-07-28 13:43:33 +0000

    Add ability to view a products additional images

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, AdditionalImages
 
 
 def all_products(request):
@@ -16,9 +16,11 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to render individual product details """
     product = get_object_or_404(Product, pk=product_id)
+    additional_images = product.additionalimages_set.all()
 
     context = {
         'product': product,
+        'additional_images': additional_images,
     }
 
     return render(request, 'products/product_detail.html', context)
