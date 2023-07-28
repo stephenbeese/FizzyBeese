e23bc69519d4c4171ac4aae7dda6d4b4d9b52c15
stephenbeese <stephenbeese1@live.co.uk>
2023-07-28 18:08:01 +0000

    Add filtering by product and fragrance categories

--- products/views.py
+++ products/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, ProductCategory, FragranceCategory
 
 
 def all_products(request):
     """ A view to render all products """
     products = Product.objects.all()
     query = None
+    product_categories = None
+    fragrance_categories = None
 
     if request.GET:
+        if 'product_categories' in request.GET:
+            categories = request.GET.getlist('product_categories')
+            products = products.filter(product_categories__name__in=categories)
+            categories = ProductCategory.objects.filter(name__in=categories)
+
+        if 'fragrance_categories' in request.GET:
+            categories = request.GET.getlist('fragrance_categories')
+            products = products.filter(fragrance_categories__name__in=categories)
+            categories = FragranceCategory.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -18,11 +30,11 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
 
     context = {
         'products': products,
         'search_term': query,
+        'current_product_categories': product_categories,
     }
 
     return render(request, 'products/products.html', context)
