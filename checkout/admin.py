be5077edb8776d1970e34f92ce214e81606bfa07
stephenbeese <stephenbeese1@live.co.uk>
2023-08-19 17:13:07 +0000

    Add order history and update checkout view to prefill the order form with user's info

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,6 +15,13 @@ class OrderAdmin(admin.ModelAdmin):
                        'order_total', 'grand_total', 'original_bag',
                        'stripe_pid')
 
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'total_weight', 'order_total', 'grand_total',
+              'original_bag', 'stripe_pid')
+
     list_display = ('order_number', 'date', 'full_name',
                     'total_weight', 'delivery_cost',
                     'order_total', 'grand_total')
