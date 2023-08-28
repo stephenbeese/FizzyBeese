ff1c65f7f0936a13ab463b96b17b28c88168f515
stephenbeese <stephenbeese1@live.co.uk>
2023-08-28 17:28:30 +0000

    Add 404 error handler page

--- fizzy_beese/urls.py
+++ fizzy_beese/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +33,5 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'fizzy_beese.views.handler404'
