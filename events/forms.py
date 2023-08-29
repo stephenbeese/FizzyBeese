001aa67d78188851b6a4287573497b7cb95a364c
stephenbeese <stephenbeese1@live.co.uk>
2023-08-29 18:27:43 +0000

    Remove commented out code

--- events/forms.py
+++ events/forms.py
@@ -10,13 +10,3 @@ class EventForm(forms.ModelForm):
             'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     start_time = cleaned_data.get('start_time')
-    #     end_time = cleaned_data.get('end_time')
-
-    #     if start_time >= end_time:
-    #         messages.error(request, 'The start time is later than the end time \
-    #                        Please change this and resubmit the form.')
-    #         return redirect('create_event')
