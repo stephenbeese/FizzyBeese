7f7c311659417c2e32c15a36547814e18b597dc5
Stephen Beese <stephenbeese@Stephens-MacBook-Air.local>
2024-07-30 14:47:38 +0100

    Fix home cards sizing and add shadowing to images

--- fizzy_beese/settings.py
+++ fizzy_beese/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '8000-stephenbeese-fizzybeese-uj86rszvgi6.ws-eu107.gitpod.io',
     '8000-stephenbeese-fizzybeese-uj86rszvgi6.ws-eu108.gitpod.io',
-    '8000-stephenbeese-fizzybeese-uj86rszvgi6.ws-eu114.gitpod.io']
+    '8000-stephenbeese-fizzybeese-uj86rszvgi6.ws-eu114.gitpod.io',
+    '127.0.0.1']
 
 # Application definition
 
